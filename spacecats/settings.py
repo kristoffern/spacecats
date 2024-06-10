e0a2befbd51c946c8855044ae6e60e05b66d838a
Kristoffer Nordstrom <kristoffer.nordstrom@northerntest.se>
2024-06-10 16:48:12 +0200

    added logging to crew view

--- spacecats/settings.py
+++ spacecats/settings.py
@@ -122,3 +122,21 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'app.log',
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    },
+}
--- spacecats/views.py
+++ spacecats/views.py
@@ -1,8 +1,16 @@
+import logging
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import SpaceCat
+
+logger = logging.getLogger(__name__)
 
 class CrewListView(ListView):
     model = SpaceCat
     template_name = 'spacecats/crew_list.html'
     context_object_name = 'spacecats'
+
+    def get(self, request, *args, **kwargs):
+        logger.info("Crew list view called")
+        logger.info(f"{SpaceCat.objects.count()} SpaceCats in the crew")
+        return super().get(request, *args, **kwargs)
